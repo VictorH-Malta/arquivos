be4f157bb9c98593bc952e7ca9031f2d7e07dfe9
Victor Henrique Malta <victor.fobos@gmail.com>
2021-08-11 10:20:08 -0300

    Aprendendo a utilizar o bloco using

--- Arquivos/Program.cs
+++ Arquivos/Program.cs
@@ -7,26 +7,28 @@ namespace Arquivos
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\victorhmalta\source\repos\file1.txt";
-            StreamReader sr = null;
+            string path = @"C:\Users\victorhmalta\source\repos\filex.txt";
+
             try
             {
-                //É possível simplificar sem instanciar um objeto FileStream
-                sr = File.OpenText(path);
-                while (!sr.EndOfStream) //Enquanto o arquivo não acabar, leia as linhas e armazene
+                //O bloco using permite que se termine automaticamente a execução de um objeto IDisposable: Font, FileStream, StreamReader, StreamWriter...
+                using (FileStream fs = new FileStream(path, FileMode.Open))
                 {
-                    string line = sr.ReadLine();
-                    Console.WriteLine(line);
+                    //É possível fazer em cascata e instanciar logo no início da execução
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
                 }
             }
             catch (IOException e)
             {
-                Console.WriteLine("An error occurred");
+                Console.WriteLine("An error occurred!");
                 Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                if (sr != null) sr.Close(); //Fecha o arquivo
             }
         }
     }

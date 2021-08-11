b8a900a0d091b19fac081c03bbdf534b3439d433
Victor Henrique Malta <victor.fobos@gmail.com>
2021-08-11 10:12:26 -0300

    Simplificando a leitura e impressão de um arquivo com o método OpenText()

--- Arquivos/Program.cs
+++ Arquivos/Program.cs
@@ -8,26 +8,25 @@ namespace Arquivos
         static void Main(string[] args)
         {
             string path = @"C:\Users\victorhmalta\source\repos\file1.txt";
-            FileStream fs = null;
             StreamReader sr = null;
-
             try
             {
-                fs = new FileStream(path, FileMode.Open); //Abre o caminho fornecido
-                sr = new StreamReader(fs); //Lê o conteúdo
-                string line = sr.ReadLine(); //Lê uma linha e armazena na variável
-                Console.WriteLine(line); //Imprime a variável na tela
+                //É possível simplificar sem instanciar um objeto FileStream
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream) //Enquanto o arquivo não acabar, leia as linhas e armazene
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
             }
             catch (IOException e)
             {
-                Console.WriteLine("An error occurred!");
+                Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
             }
             finally
             {
-                //É necessário fechar o arquivo manualmente por enquanto!
-                if (fs != null) fs.Close();
-                if (sr != null) sr.Close();
+                if (sr != null) sr.Close(); //Fecha o arquivo
             }
         }
     }
